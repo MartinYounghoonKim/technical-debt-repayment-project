28d13ad17fa6afad72e9557b887805c334b06a03
martinyounghoonkim <hoons0131@gmail.com>
2019-09-17 23:32:19 +0900

    출석부 구현에 대한 홈워크 샘플 코드 작성

---
+++ kotlin/kotlin-in-action/src/Homework/Attendance.kt
@@ -0,0 +1,13 @@
+package Homework
+
+data class Student (val name: String,
+                    val koreanScore: Int,
+                    val englishScore: Int,
+                    val mathScore: Int)
+
+fun main(args: Array<String>) {
+    val students = listOf(Student("Martin", 80, 90, 75),
+            Student("Irene", 40, 80, 90),
+            Student("Hong", 30, 100, 100))
+    println(students)
+}
---
+++ kotlin/kotlin-in-action/src/Homework/README.md
@@ -0,0 +1,10 @@
+data class Student
+- name
+- kor
+- eng
+- math
+
+** 하나 이상의 root function
+** 우대: 클로저 이용 
+
+문제 1. 평균이 70이상이고 과락(한과목이 50점 이하)이 없는 학생
