895f11bbe3bb543d574942b1eddb3220c3207312
martinyounghoonkim <hoons0131@gmail.com>
2019-09-18 23:38:07 +0900

    [출석부 홈워크] filter 메소드 적용해보기

--- kotlin/kotlin-in-action/src/Homework/Attendance.kt
+++ kotlin/kotlin-in-action/src/Homework/Attendance.kt
@@ -9,5 +9,6 @@ fun main(args: Array<String>) {
     val students = listOf(Student("Martin", 80, 90, 75),
             Student("Irene", 40, 80, 90),
             Student("Hong", 30, 100, 100))
-    println(students)
+    println(students.filter { it.koreanScore > 50 && it.englishScore > 50 && it.mathScore > 50 })
+
 }
